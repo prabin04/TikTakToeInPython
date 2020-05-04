85c15b2f45b62fc393649078ab198fceb23979b4
Prabin Parajuli <prabin.b9@gmail.com>
2020-05-04 12:26:48 +0200

    Update nested_loops.py

--- nested_loops.py
+++ nested_loops.py
@@ -6,9 +6,9 @@
 # ----3
 # | | 4
  
-for rows in range(5):
+for rows in range(5): # 0, 1, 2, 3, 4
     if rows%2 == 0:
-        for column in range(1,6):
+        for column in range(1,6): #1, #2, #3, #4, #5
             if column%2 == 1:
                 if column != 5:
                     print(" ",end="")
